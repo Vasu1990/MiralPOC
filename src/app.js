0aedace9acfddc94ff4ae1ba6f81436f96babd93
SAPIENT\vnagp3 <vnagpal2@sapient.com>
2017-06-06 17:49:19 +0530

    added export on ServerComponents variable

---
+++ src/app.js
@@ -0,0 +1,9 @@
+// import these files from their respective folders
+import React from 'react'
+import ReactDOM from 'react-dom';
+import style from '../public/assets/css/style.css';
+import component1 from './Components/Cart/ReactComponent1';
+import component2 from './Components/Cart/ReactComponent2';
+
+var ServerComponents = {component1 , component2}
+export  default ServerComponents;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
-//import these files from their respective folders
-import React from 'react'
-import ReactDOM from 'react-dom';
-import style from '../public/assets/css/style.css';
-import component1 from './Components/Cart/ReactComponent1';
-import component2 from './Components/Cart/ReactComponent2';
+// import these files from their respective folders
+import ServerComponent from './app.js';
